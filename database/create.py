1346dc206c83e35a997417af04117a166e9daf71
Manuel Cánepa <manuelcanepa@gmail.com>
2020-05-22 18:57:01 -0300

    Cambios para la creacion de datos

--- database/create.py
+++ database/create.py
@@ -1,3 +1,4 @@
+import sys, traceback
 import os
 from os import walk
 import mysql.connector
@@ -12,9 +13,8 @@ def conn_root():
     global cursor_root
     try:
         cnx_root = mysql.connector.connect(user='root',
-                                password='neheik',
-                                host='localhost')
-
+                password='root',
+                host='tssi-db')
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
@@ -23,8 +23,8 @@ def conn_root():
         else:
             print(err)
     else:
-        cursor_root = cnx_tusi.cursor()
-        print("Connected as tusi3. Connected: ", str(cnx_tusi.is_connected()) ,datetime.now())
+        cursor_root = cnx_root.cursor()
+        print("Connected as tusi3. Connected: ", str(cnx_root.is_connected()) ,datetime.now())
 
 cnx_tusi = None
 cursor_tusi = None
@@ -35,7 +35,7 @@ def conn_tusi():
     try:
         cnx_tusi = mysql.connector.connect(user='tusi3',
                                 password='tusi3-2020',
-                                host='localhost',
+                                host='tssi-db',
                                 database='tusi3')
         
     except mysql.connector.Error as err:
@@ -51,19 +51,18 @@ def conn_tusi():
     #for x in dir(cnx_tusi):
     #    print('---',x)
 
-try:
-  cnx = mysql.connector.connect(user='scott',
-                                database='employ')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cnx.close()
-
+# try:
+#   cnx = mysql.connector.connect(user='scott',
+#                                 database='employ')
+# except mysql.connector.Error as err:
+#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#     print("Something is wrong with your user name or password")
+#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#     print("Database does not exist")
+#   else:
+#     print(err)
+# else:
+#   cnx.close()
 
 
 tablas = [
@@ -82,7 +81,6 @@ def close_root_db():
     cnx_root.close
     print("Disconnected as root.", datetime.now())
 
-
 def close_tusi_db():
     cursor_tusi.close()
     cnx_tusi.close()
@@ -91,24 +89,32 @@ def close_tusi_db():
 def exec_root_sql(sql):
     try:
         for cmd in sql:
-            #cursor_root.execute(cmd)
-            cursor_root.execute(cmd, multi=True)
+            if len(cmd) == 0:
+                continue
+            cursor_root.execute(cmd)
+            # cursor_root.execute(cmd, multi=True)
     except Exception as e:
         print("Error de proceso: ", str(e))
+        traceback.print_exc()
+        exit(1)
 
 def exec_tusi_sql(sql):
     try:
         for cmd in sql:
-            if len(cmd) == 0:
+            # print(dir(cmd))
+            if len(cmd.strip()) == 0:
                 continue
-            print(cmd)
-            cursor_tusi.execute(cmd + ';', multi=True)
+            # print(cmd)
+            cursor_tusi.execute(cmd)
+            # cursor_tusi.execute(cmd + ';', multi=True)
             cnx_tusi.commit()
     except Exception as e:
         print("Error de proceso: ", str(e))
+        traceback.print_exc()
+        exit(1)
 
 def get_sql(sql):
-    print('Processing ' + sql + ': ' + str(datetime.now()))
+    # print('Processing ' + sql + ': ' + str(datetime.now()))
     with open(sql, 'r') as d:
         data = d.read()
     return data.split(';')
@@ -129,29 +135,32 @@ def create_tables():
     dir_ = 'tables'
     for t in tablas:
         tab = dir_ + '/' + t + '.sql'
-        print(get_sql(tab))
+        print('Creacion de tabla: ' + t)
+        exec_tusi_sql(get_sql(tab))
 
 def load_tables():
     dir_ = 'load'
     for t in tablas:
         ldr = dir_ + '/' + t.lower() + '.sql'
-        print(get_sql(ldr))
+        print('Datos de: ' + t.lower())
+        exec_tusi_sql(get_sql(ldr))
 
 def create_procedures():
     dir_ = 'procedures'
     for m in metodos:
         for t in tablas:
             sp = dir_ + '/' + m + '/' + m + t.lower() + '.sql'
-            print(get_sql(sp))
+            print('SP: ' + m + t.lower())
+            exec_tusi_sql(get_sql(sp))
 
-#conn_root()
-#create_database()
-#create_user()
-#close_root_db()
+conn_root()
+create_database()
+create_user()
+close_root_db()
 conn_tusi()
 create_datosbase()
-#load_datosbase()
-#create_tables()
-#load_tables()
-#create_procedures()
+load_datosbase()
+create_tables()
+load_tables()
+create_procedures()
 close_tusi_db()
